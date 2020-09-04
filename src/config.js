5152e135abcefde3354363e2f5302886333867d2
BeardKoda <akinsuyi.joshua84@gmail.com>
2020-09-04 16:27:12 +0100

    All

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config ={
-	API: process.env.UAPI || 'http://test-api.universalapi.net/api/v1/universal',
+	API: process.env.UAPI || 'https://test-api.universalapi.net/api/v1/universal',
 	secret: process.env.secret_key || 'PRTSECK_TEST_E5A273E9EACC1A40AFA140BC00BA4A'
 }
 module.exports = config;
